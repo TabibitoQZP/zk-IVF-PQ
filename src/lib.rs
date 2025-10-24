aff7751d7250cfa17cb8df50c02edcfaaddf4251
tabibitoqzp <tabibitoqzp@gmail.com>
2025-10-24 10:29:42 +0800

    some old gadget deprecated

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 // 有pub会给外部crate用, 无pub是给内部crate用
-pub mod common_gadgets;
+// pub mod common_gadgets;
 pub mod hash_gadgets;
 pub mod ivf_flat;
-pub mod ivfpq;
-pub mod nn_gadgets;
+// pub mod ivfpq;
+// pub mod nn_gadgets;
 pub mod prelude;
 pub mod utils;
 
 use crate::hash_gadgets::hash_u64;
 use crate::ivf_flat::proof::ivf_flat_proof;
-use crate::nn_gadgets::nn_prove;
+// use crate::nn_gadgets::nn_prove;
 use pyo3::prelude::*;
 
 #[pyfunction]
@@ -17,16 +17,16 @@ fn single_hash(input: Vec<u64>) -> PyResult<u64> {
     Ok(hash_u64(input))
 }
 
-#[pyfunction]
-fn py_nn_prove(
-    src_vecs: Vec<Vec<u64>>,
-    query: Vec<u64>,
-    root: u64,
-    sorted_idx_dis: Vec<Vec<u64>>,
-) -> PyResult<bool> {
-    let corr = nn_prove(src_vecs, query, root, sorted_idx_dis).is_ok();
-    Ok(corr)
-}
+// #[pyfunction]
+// fn py_nn_prove(
+//     src_vecs: Vec<Vec<u64>>,
+//     query: Vec<u64>,
+//     root: u64,
+//     sorted_idx_dis: Vec<Vec<u64>>,
+// ) -> PyResult<bool> {
+//     let corr = nn_prove(src_vecs, query, root, sorted_idx_dis).is_ok();
+//     Ok(corr)
+// }
 
 #[pyfunction]
 fn py_ivf_flat_proof(
@@ -59,7 +59,7 @@ fn batch_hash(inputs: Vec<Vec<u64>>) -> PyResult<Vec<u64>> {
 fn zk_IVF_PQ(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(single_hash, m)?)?;
     m.add_function(wrap_pyfunction!(batch_hash, m)?)?;
-    m.add_function(wrap_pyfunction!(py_nn_prove, m)?)?;
+    // m.add_function(wrap_pyfunction!(py_nn_prove, m)?)?;
     m.add_function(wrap_pyfunction!(py_ivf_flat_proof, m)?)?;
     Ok(())
 }
